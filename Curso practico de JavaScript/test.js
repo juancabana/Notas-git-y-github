51870cc62d13df7b7772a5180dbe14d2c8656cad
juancabana <juand.cabanat@unac.edu.co>
2022-12-18 19:50:27 -0500

    Begin ECMA Script

---
+++ Curso practico de JavaScript/test.js
@@ -0,0 +1,22 @@
+let nombre;
+let apellido;
+let nickname;
+let nombre_de_usuario_en_Platzi;
+let edad;
+let correo_electrónico;
+let mayor_de_edad;
+let dinero_ahorrado;
+let deudas;
+
+nombre = "Juan David";
+apellido = "Cabana Trejos";
+nickname = "CabanaJuan"
+dinero_ahorrado = 10000000;
+deudas = 6000000;
+
+const calc = (name, lastName, nickname) => {
+    console.log(`Hola, no me llames ${name} ${lastName}, prefiero que me llamen ${nickname}`);
+}
+
+calc(nombre, apellido, nickname);
+
