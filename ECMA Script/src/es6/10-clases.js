6f25883414e890a6c1da347eca55b9facd4b8601
juancabana <juand.cabanat@unac.edu.co>
2022-12-20 10:10:55 -0500

    Advances

--- ECMA Script/src/es6/10-clases.js
+++ ECMA Script/src/es6/10-clases.js
@@ -1,14 +1,64 @@
 // Clase
-class User {
+class user {
+    // Metodos
+    saludo(name = 'usuario') {
+        return `Hola ${name}`;
+    }
+};
+
+// Instancias
+const juan = new user();
+const anonimo = new user();
+console.log(juan.saludo('Juan'));
+console.log(anonimo.saludo());
+
+
+class userClass {
+    // constructor
+    constructor(name) {
+        this.name = name;
+        // console.log('Nuevo usuario');
+    }
+    speak() {
+        return 'Hello';
+    }
+    saludo() {
+        return `${this.speak()} ${this.name}`;
+    }
 
 }
 
-// Instancia de la clase
-// const newUser = new User();
+const david = new userClass('David');
+console.log(david.saludo())
 
-class user {
-    // Metodos
-    saludo() {
+// setters and getters
+
+class userSetAndGet {
+    // constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // metodos
+    speak() {
         return 'Hello';
     }
-};
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+
+    }
+
+    get uAge() {
+        return this.age
+    }
+    set uAge(n) {
+        this.age = n
+    }
+
+}
+
+const developer = new userSetAndGet('Juan', 19);
+console.log(developer.age);
+console.log(developer.uAge);
+console.log(developer.uAge = 20);
