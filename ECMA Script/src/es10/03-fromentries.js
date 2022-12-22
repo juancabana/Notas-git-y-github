6296d2071de4dc5c7b220e0fc7b4ef5a1e1bc4d2
juancabana <juand.cabanat@unac.edu.co>
2022-12-22 10:35:39 -0500

    Commit es11

---
+++ ECMA Script/src/es10/03-fromentries.js
@@ -0,0 +1,3 @@
+const entries = new Map([['name', 'Oscar'], ['age', 34]]);
+let transformation = Object.fromEntries(entries);
+console.log(transformation);
---
+++ ECMA Script/src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const bigNumber = 12345678901234567896n;
+const bigNumber2 = BigInt(12345678901234567896);
+
+console.log(bigNumber);
+console.log(bigNumber2);
